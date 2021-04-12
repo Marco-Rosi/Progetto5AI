ccb134e5d3a82f5aac018dcd3d7509342d5f2a72
Monica Ciuchetti <monica.ciuchetti@gmail.com>
2021-04-12 11:45:02 +0200

    modifica file main

--- src/prova5ai/Prova5AI.java
+++ src/prova5ai/Prova5AI.java
@@ -16,6 +16,7 @@ public class Prova5AI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("CIAO a tutti");
     }
     
 }
