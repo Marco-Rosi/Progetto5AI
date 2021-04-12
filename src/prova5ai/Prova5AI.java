2419da8a7f2baace87828d2a2da42a2d7ce64c27
Monica Ciuchetti <monica.ciuchetti@gmail.com>
2021-04-12 11:31:22 +0200

    primo allineamento

---
+++ src/prova5ai/Prova5AI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prova5ai;
+
+/**
+ *
+ * @author monica
+ */
+public class Prova5AI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
